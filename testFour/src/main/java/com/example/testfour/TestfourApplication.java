345548fc79f95808da30f6d07d0ee4571c56f94c
chenyangjln <18292849096@163.com>
2019-12-03 16:47:39 +0800

    druid

---
+++ testFour/src/main/java/com/example/testfour/TestfourApplication.java
@@ -0,0 +1,13 @@
+package com.example.testfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestfourApplication.class, args);
+    }
+
+}
---
+++ testFour/src/test/java/com/example/testfour/TestfourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testfour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
