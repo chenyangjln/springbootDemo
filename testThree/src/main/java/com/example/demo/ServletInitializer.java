adcda9f19ff77bd880c5e7352bc06943d31b8d0c
chenyangjln <18292849096@163.com>
2019-12-03 15:18:53 +0800

    springDateJPA

---
+++ testThree/src/main/java/com/example/demo/ServletInitializer.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 现在JavaConfig配置方式在逐步取代xml配置方式。
+ * 而WebApplicationInitializer可以看做是Web.xml的替代，它是一个接口。
+ * 通过实现WebApplicationInitializer，在其中可以添加servlet，listener等，
+ * 在加载Web项目的时候会加载这个接口实现类，从而起到web.xml相同的作用。
+ *
+ * SpringBootServletInitializer这个类是springboot提供的web程序初始化的入口，
+ * 当我们使用外部容器（后期文章讲解使用外部tomcat如何运行项目）运行项目时会自动加载并且装配。
+ * @author Alex
+ * @create 2019-12-01 16:53
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    /**
+     * 实现了SpringBootServletInitializer的子类需要重写一个configure方法，
+     * 方法内自动根据TestApplication.class的类型创建一个SpringApplicationBuilder交付给springboot框架来完成初始化运行配置
+     * @param application
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+}
---
+++ testThree/src/main/java/com/example/demo/jpa/UserJpa.java
@@ -0,0 +1,22 @@
+package com.example.demo.jpa;
+
+import com.example.demo.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.io.Serializable;
+
+/**
+ * 我们UserJPA继承了JpaRepository接口（SpringDataJPA提供的简单数据操作接口）、
+ * JpaSpecificationExecutor（SpringDataJPA提供的复杂查询接口）、Serializable（序列化接口）。
+ * 我们并不需要做其他的任何操作了，因为SpringBoot以及SpringDataJPA会为我们全部搞定，
+ * SpringDataJPA内部使用了类代理的方式让继承了它接口的子接口都以spring管理的Bean的形式存在，
+ * 也就是说我们可以直接使用@Autowired注解在spring管理bean使用
+ * @author Alex
+ * @create 2019-12-02 19:35
+ */
+public interface UserJpa extends
+        JpaRepository<UserEntity,Long> ,
+        JpaSpecificationExecutor <UserEntity>,
+        Serializable {
+}
